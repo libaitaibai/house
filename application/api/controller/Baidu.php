5b3cb63f06b2b9d227b1d955f4c310821d435668
cxg <942315912@qq.com>
2019-10-13 10:10:18 +0800

    feat:百度翻译api

---
+++ application/api/controller/Baidu.php
@@ -0,0 +1,118 @@
+<?php
+
+
+namespace app\api\controller;
+
+
+class Baidu
+{
+    //翻译入口,调用这个
+    function translate($query, $from='auto', $to='en')
+    {
+        $fy_url    =  'http://api.fanyi.baidu.com/api/trans/vip/translate';
+        $app_id =   '20191003000338950';
+        $sec_key =   '9Rg14u1HD3XyDWsN_YB6';
+        $args = array(
+            'q' => $query,
+            'appid' => $app_id,
+            'salt' => rand(10000,99999),
+            'from' => $from,
+            'to' => $to,
+        );
+        $args['sign'] = $this->buildSign($query, $app_id, $args['salt'], $sec_key);
+        $ret = $this->call($fy_url, $args);
+        $ret = json_decode($ret, true);
+        return json($ret);
+    }
+
+//加密
+    function buildSign($query, $appID, $salt, $secKey)
+    {/*{{{*/
+        $str = $appID . $query . $salt . $secKey;
+        $ret = md5($str);
+        return $ret;
+    }/*}}}*/
+
+//发起网络请求
+    function call($url, $args=null, $method="post", $testflag = 0, $timeout = 10, $headers=array())
+    {/*{{{*/
+        $ret = false;
+        $i = 0;
+        while($ret === false)
+        {
+            if($i > 1)
+                break;
+            if($i > 0)
+            {
+                sleep(1);
+            }
+            $ret = $this->callOnce($url, $args, $method, false, $timeout, $headers);
+            $i++;
+        }
+        return $ret;
+    }/*}}}*/
+
+    function callOnce($url, $args=null, $method="post", $withCookie = false, $timeout = 10, $headers=array())
+    {/*{{{*/
+        $ch = curl_init();
+        if($method == "post")
+        {
+            $data = $this->convert($args);
+            curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+            curl_setopt($ch, CURLOPT_POST, 1);
+        }
+        else
+        {
+            $data = convert($args);
+            if($data)
+            {
+                if(stripos($url, "?") > 0)
+                {
+                    $url .= "&$data";
+                }
+                else
+                {
+                    $url .= "?$data";
+                }
+            }
+        }
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_TIMEOUT, $timeout);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        if(!empty($headers))
+        {
+            curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+        }
+        if($withCookie)
+        {
+            curl_setopt($ch, CURLOPT_COOKIEJAR, $_COOKIE);
+        }
+        $r = curl_exec($ch);
+        curl_close($ch);
+        return $r;
+    }/*}}}*/
+
+    function convert(&$args)
+    {/*{{{*/
+        $data = '';
+        if (is_array($args))
+        {
+            foreach ($args as $key=>$val)
+            {
+                if (is_array($val))
+                {
+                    foreach ($val as $k=>$v)
+                    {
+                        $data .= $key.'['.$k.']='.rawurlencode($v).'&';
+                    }
+                }
+                else
+                {
+                    $data .="$key=".rawurlencode($val)."&";
+                }
+            }
+            return trim($data, "&");
+        }
+        return $args;
+    }/*}}}*/
+}
