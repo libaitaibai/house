01d30c86ff0720f7c45b94f885748767567be537
yang.lan <496713192@qq.com>
2019-10-13 09:57:57 +0800

    中英文

--- application/api/controller/Applet.php
+++ application/api/controller/Applet.php
@@ -16,13 +16,39 @@ use app\admin\model\CmsHouse;
 
 class Applet extends Base
 {
+    public $http = 'http://';
+
     public function index()
     {
+        $action = input('do');
+        $data =[];
+        switch ($action) {
+            case 'GetSysInit':
+                $data = $this->show();
+                break;
+            case 1:
+                echo "i equals 1";
+                break;
+            case 2:
+                echo "i equals 2";
+                break;
+        }
+//        echo '<pre>';var_dump($action);exit;
+        return $this->json($data);
+    }
+
+    /**
+     * 首页展示信息
+     * @return array
+     */
+    public function show()
+    {
+        //播放图片
         $list=db('ads_data')->where(['aid'=>12,'show'=>1])->order(['sort'=>'desc','id'=>'desc'])->limit(10)->select();
-        $list[0]['thumb'] = "http://www.happy.com/uploads/20190216/6b456af2c8142c6b4192ab7e43888c24.jpg";
-        $list[1]['thumb'] = "http://www.happy.com/uploads/20190216/6881f0247739f5882eda3590c0ac0bd8.jpg";
-//        echo'<pre>';var_dump($list);exit;
-//        $list='[{"id":"16","weid":"52","advname":"B1","link":"","thumb":"https:\/\/api.site100.cn\/attachment\/images\/52\/2019\/03\/eMqmMDu4DdtM768zttTl4z6TUMdWt4.jpg","displayorder":"0","enabled":"1","toway":"","appid":"","type":"0"},{"id":"17","weid":"52","advname":"B2","link":"","thumb":"https:\/\/api.site100.cn\/attachment\/images\/52\/2019\/03\/hmm0hFLO4av4a2OM33S92qyAF4cC5h.jpg","displayorder":"0","enabled":"1","toway":"","appid":"","type":"0"},{"id":"18","weid":"52","advname":"B3","link":"","thumb":"https:\/\/api.site100.cn\/attachment\/images\/52\/2019\/03\/C0J3c9I9A64q6dgl3bai0qALnCrLjQ.jpg","displayorder":"0","enabled":"1","toway":"","appid":"","type":"0"}]';
+        array_walk($list,function(&$val){
+            $val['thumb'] = $this->http.$_SERVER['HTTP_HOST'].$val['thumb'];
+        });
+
         $data = [
             'banner'=>($list),
             'midadv'=>[],
@@ -37,6 +63,6 @@ class Applet extends Base
             'cityinfo'=>[],
             'article'=>[],
         ];
-        return $this->json($data);
+        return $data;
     }
 }
