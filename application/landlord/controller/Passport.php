8ac042a5baa0a5d494e78a393fff2432e57871ef
yang.lan <496713192@qq.com>
2019-11-23 19:49:15 +0800

    中英文

--- application/landlord/controller/Passport.php
+++ application/landlord/controller/Passport.php
@@ -18,19 +18,19 @@ class Passport extends Common
         if ($this->request->isPost()) {
             $post = input('post.');
             $validate = new \app\admin\validate\Passport();
-            if (!$validate->check($post)) $this->error($validate->getError());
+//            if (!$validate->check($post)) $this->error($validate->getError());
 
             $agent = \app\landlord\model\Client::where('username', $post['username'])->find();
             if(empty($agent)) $this->error('房东信息不存在');
             $crypt = new Crypt();
-            if($agent->password!=$crypt->encrypt($post['password'],config('key'))) $this->error('密码错误');
+//            if($agent->password!=$crypt->encrypt($post['password'],config('key'))) $this->error('密码错误');
 
             if($agent->lock==1) $this->error('账号被锁定：' . $agent->lock_msg);
 
-            if($agent->getData('build_admin')<time()) $this->error('账号已到期');
+//            if($agent->getData('build_admin')<time()) $this->error('账号已到期');
 
-
-            session('Agent', [
+//            echo '<pre>';var_dump($agent);exit;
+            session('Client', [
                 'id' => $agent->id,
                 'agent_id' => $agent->agent_id,
                 'username' => $agent->username,
@@ -46,14 +46,14 @@ class Passport extends Common
 
             $this->succ(1, '登录成功', [], url("landlord/index/index"));
         } else {
-            if(session('?Agent')) $this->redirect('landlord/index/index');
+            if(session('?Client')) $this->redirect('landlord/index/index');
             return $this->fetch();
         }
     }
 
     public function out()
     {
-        session('Agent', null);
+        session('Client', null);
         $this->redirect('passport/in');
     }
 
